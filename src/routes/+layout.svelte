428b13087024950570608c8183fcb4f5743a990d
DanielFernandoVerdecia <123104610+DanielFernandoVerdecia@users.noreply.github.com>
2025-11-20 00:21:38 -0500

    Agregamos Icono de una M Mecamblock  Favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/icon_mecamblock.png';
 	import "bootstrap/dist/css/bootstrap.min.css";
 	
 	let { children } = $props();
